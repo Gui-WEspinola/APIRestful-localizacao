fe48450e4bbdb992979866f8da16d79d0e763e45
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-09-09 15:37:40 -0300

    101. Variantes do Like

--- src/main/java/io/github/guiwespinola/localizacao/domain/repository/CidadeRepository.java
+++ src/main/java/io/github/guiwespinola/localizacao/domain/repository/CidadeRepository.java
@@ -2,6 +2,7 @@ package io.github.guiwespinola.localizacao.domain.repository;
 
 import io.github.guiwespinola.localizacao.domain.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,11 +10,8 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
     List<Cidade> findByNome(String nome);
 
-    List<Cidade> findByNomeStartingWith(String nome);
-
-    List<Cidade> findByNomeEndingWith(String nome);
-
-    List<Cidade> findByNomeContaining(String nome);
+    @Query(" select c from Cidade c where lower(c.nome) like lower(:nome) ")
+    List<Cidade> findByNomeLike(String nome);
 
     List<Cidade> findByHabitantes(Long habitantes);
 
